8c9a22e664e819bf687d0626cc495295b4afdde1
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-03-12 23:21:05 -0700

    add example templating to hello.html

--- project_manager/views.py
+++ project_manager/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from .models import Project, Phase
 # Create your views here.
 def hello(request):
     # users = User.objects.all()
+    projects = Project.objects.all()
+    phases = Phase.objects.all()
+
     context = {
-        'hello': "hello",
+        'projects': projects,
+        'phases': phases,
     }
     return render(request, 'pages/hello.html', context)
