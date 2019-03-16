58527cf41b8213e192942939ac65631137524335
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-03-16 15:51:41 -0700

    project phases limited to only related phases in project_list

--- project_manager/views.py
+++ project_manager/views.py
@@ -6,33 +6,7 @@ from .models import Project, Phase
 
 
 def hello(request):
-    # users = User.objects.all()
-    # print(Project.objects.all()[1])
-    # print(Phase.objects.all()[1])
-    # print(request)
-    
 
-    # output = {}
-    # p_id = retrieve_project_id(request.)
-
-    # context = {
-    #     'first_project': {
-    #         'name': name_proj,
-    #         'proj_id': proj_id,
-    #         'start_date': start_date,
-    #         'end_date': end_date,
-    #         'phases': [
-    #                 'phase 1': {
-    #                     'name': phase_name,
-    #                     'start': start,
-    #                     'end': end,
-    #                 },
-    #                 'phase 2',
-    #                 'phase 3',
-    #         ]
-    #         }
-
-    # }
     projects = Project.objects.all()
     
     phases = Phase.objects.all()
@@ -43,35 +17,16 @@ def hello(request):
         proj_dict["address"] = project.address
         proj_dict["start_date"] = project.start_date
         proj_dict["end_date"] = project.end_date
-        proj_dict["phases"] = [phase for phase in phases]
+        proj_dict["phases"] = project.phases.all()
         project_list.append(proj_dict)
-
-        context = {
-            'projects_list': project_list
-        }
-    
-
-
-        # print('project:', project)
-        # print('all phases:' project.phases.all())
-    print('projects list:', project_list)
-        # context["project_name"] = project.name
-        # context["address"] = project.address
-        
-
-
-
-    # for item in first_project.phases
-    #     for whatevs in item.level2.level3.level4
-    #     <p>item.name</p>
-
-    # print(Project.objects.count())
-    # for project in projects:
-    #     print(project.id)
-    #
+        print('project:', project, '   phases:',proj_dict["phases"])
 
     context = {
-        # 'projects': projects,
-        # 'phases': phases,
+        'projects_list': project_list
     }
+
+
+
+
+    print('projects list:', project_list)
     return render(request, 'pages/hello.html', context)
